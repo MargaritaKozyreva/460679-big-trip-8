d9731b073aa5ed8ed0e37db398806cf46859821f
Margarita <35699592+MargaritaKozyreva@users.noreply.github.com>
2019-03-24 12:21:01 +0300

    fix bugs

--- src/constants.js
+++ src/constants.js
@@ -57,8 +57,8 @@ const getPrice = () => {
   }, {});
 };
 
-export const OFFERS = getOffers();
-export const PRICE = getPrice();
+export const Offer = getOffers();
+export const Price = getPrice();
 export const DESCRIPTION = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
 export const DESCRIPTIONS_COUNT = 3;
 export const TIME_START = `9:00, 10:00, 11:00, 12:00, 13:00, 14:00, 15:00, 16:00, 17:00, 18:00, 19:00, 20:00, 21:00, 22:00, 23:00, 00:00`;
